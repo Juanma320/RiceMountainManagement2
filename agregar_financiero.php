a0ab726d7cab2a4e407f87d22cffac19195c9534
Juanma320 <juanma320@gmail.com>
2024-04-19 21:18:54 -0500

    Agregar_financiero Styles

--- agregar_financiero.php
+++ agregar_financiero.php
@@ -1,17 +1,22 @@
 <?php
-include('includes/includes.php');
-include('includes/funciones.php');
+include ('includes/includes.php');
+include ('includes/funciones.php');
 $row = obtenerDatosUsuario($conexion, $_SESSION['NombreUsuario']);
+// Verificar si el usuario tiene el rol de administrador
+if ($_SESSION['RolID'] != 1) {
+    // Si no es administrador, redirigir a la página de inicio
+    header('Location: login.php');
+    exit();
+}
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // Obtener datos del formulario
     $nombreUsuario = mysqli_real_escape_string($conexion, $_POST['nombre_usuario']);
     $contra = password_hash($_POST['contra'], PASSWORD_DEFAULT);
     $documentoIdentidad = mysqli_real_escape_string($conexion, $_POST['documento_identidad']);
     $correo = mysqli_real_escape_string($conexion, $_POST['correo']);
-    
+
     // Hashear la contraseña
     $hashedContra = password_hash($contra, PASSWORD_DEFAULT);
-
 
     // Insertar nuevo usuario financiero en la base de datos
     $query = "INSERT INTO Usuarios (NombreUsuario, Contra, DocumentoIdentidad, Correo, RolID, Activo, FechaUltimaActividad)
@@ -28,33 +33,143 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Crear Financiero</title>
+
+    <link rel="icon" href="img/mdb-favicon.ico" type="image/x-icon" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" />
+    <link rel="stylesheet" href="css/mdb.min.css" />
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.22/css/dataTables.bootstrap4.min.css">
 </head>
-<body>
 
-    <?php include('includes/navbar.php'); ?>
+<body class="gradient-custom-1">
 
-    <h1>Crear Financiero</h1>
+    <?php include ('includes/navbar.php'); ?>
 
-    <form method="POST" action="">
-        <label for="nombre_usuario">Nombre de Usuario:</label><br>
-        <input type="text" id="nombre_usuario" name="nombre_usuario" required><br>
+    <style>
+        .gradient-custom-1 {
+            height: 100vh;
 
-        <label for="contra">Contraseña:</label><br>
-        <input type="password" id="contra" name="contra" required><br>
+            /* fallback for old browsers */
+            background: #EEEEEE;
+        }
 
-        <label for="documento_identidad">Documento de Identidad:</label><br>
-        <input type="text" id="documento_identidad" name="documento_identidad" required><br>
+        .mask-custom {
+            background: rgba(24, 24, 16, .2);
+            border-radius: 2em;
+            backdrop-filter: blur(25px);
+            border: 2px solid rgba(255, 255, 255, 0.05);
+            background-clip: padding-box;
+            box-shadow: 10px 10px 10px rgba(46, 54, 68, 0.03);
+        }
 
-        <label for="correo">Correo Electrónico:</label><br>
-        <input type="email" id="correo" name="correo" required><br>
+        .near-moon-gradient {
+            background: radial-gradient(circle at 10% 20%, rgb(64, 84, 178) 0%, rgb(219, 2, 234) 90%);
+        }
+    </style>
 
-        <input type="submit" value="Crear Financiero">
-        <input type="button" value="Cancelar" onclick="history.go(-1);">
-    </form>
+    <div class="mx-4 my-4">
+        <a class="btn text-white btn-lg btn-floating" data-mdb-ripple-init style="background-color: #ac2bac;"
+            role="button" onclick="window.location.href='gestionfinancieros.php'">
+            <i class="fas fa-angle-left"></i>
+        </a>
+
+        <div class="container">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                    <div class="card near-moon-gradient form-white" style="border-radius: 1rem;">
+                        <div class="card-body p-5">
+                            <form method="POST" action="">
+                                <h3 class="text-center indigo-text font-bold py-4 fw-bold text-uppercase">
+                                    <strong style="color: #fff">Agregar Financiero</strong>
+                                </h3>
+                                <div class="mb-4">
+                                    <div class="d-flex flex-nowrap">
+                                        <div class="order-0 col-md-1 d-flex align-items-center">
+                                            <i class="far fa-user prefix white-text" style="color: #fff"></i>
+                                        </div>
+                                        <div data-mdb-input-init class="order-1 form-outline form-white">
+                                            <input type="text" id="nombre_usuario" class="form-control form-control-lg"
+                                                name="nombre_usuario" required />
+                                            <label class="form-label" for="nombre_usuario">Nombre de usuario</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="mb-4">
+                                    <div class="d-flex flex-nowrap">
+                                        <div class="order-0 col-md-1 d-flex align-items-center">
+                                            <i class="fa-regular fa-eye-slash white-text" style="color: #fff"></i>
+                                        </div>
+                                        <div data-mdb-input-init class="order-1 form-outline form-white">
+                                            <input type="password" id="contra" class="form-control form-control-lg"
+                                                name="contra" required />
+                                            <label class="form-label" for="contra">Contraseña</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="mb-4">
+                                    <div class="d-flex flex-nowrap">
+                                        <div class="order-0 col-md-1 d-flex align-items-center">
+                                            <i class="far fa-address-card white-text" style="color: #fff"></i>
+                                        </div>
+                                        <div data-mdb-input-init class="order-1 form-outline form-white">
+                                            <input type="text" id="documento_identidad"
+                                                class="form-control form-control-lg" name="documento_identidad" />
+                                            <label class="form-label" for="documento_identidad">Documento de
+                                                identidad</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="mb-4">
+                                    <div class="d-flex flex-nowrap">
+                                        <div class="order-0 col-md-1 d-flex align-items-center">
+                                            <i class="far fa-envelope prefix white-text" style="color: #fff"></i>
+                                        </div>
+                                        <div data-mdb-input-init class="order-1 form-outline form-white">
+                                            <input type="email" id="correo" class="form-control form-control-lg"
+                                                name="correo" required />
+                                            <label class="form-label" for="correo">Correo electrónico</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="d-flex flex-row-reverse justify-content-center">
+                                    <div class="order-0 p-2">
+                                        <button data-mdb-ripple-init type="submit" class="btn btn-light">
+                                            <i class="fas fa-user-plus pe-2"></i>Agregar Financiero</button>
+                                    </div>
+                                    <div class="order-1 p-2">
+                                        <button data-mdb-ripple-init type="button" class="btn btn-danger"
+                                            onclick="window.location.href='gestionfinancieros.php'">
+                                            <i class="fas fa-ban pe-2"></i>Cancelar
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 </body>
+
 </html>
+
+<script type="text/javascript" src="js/mdb.umd.min.js"></script>
+<script type="text/javascript"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap4.min.js"></script>
+<script>
+    import { Ripple, initMDB } from "mdb-ui-kit";
+
+    initMDB({ Ripple });
+</script>
