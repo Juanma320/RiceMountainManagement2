a76d0c12f9a6cc0976ef36051322a86ab2df2bf8
Juanma320 <juanma320@gmail.com>
2024-04-23 10:18:22 -0500

    Editarproveedor Styles

--- editarproveedor.php
+++ editarproveedor.php
@@ -1,10 +1,8 @@
 <?php
-// Archivo: editarproveedor.php
-
-// Incluir configuraciones y funciones comunes
-include('includes/includes.php');
-include('includes/funciones.php');
+include ('includes/includes.php');
+include ('includes/funciones.php');
 $row = obtenerDatosUsuario($conexion, $_SESSION['NombreUsuario']);
+
 // Verificar si el usuario tiene el rol de administrador
 if ($_SESSION['RolID'] != 1) {
     // Si no es administrador, redirigir a la página de inicio
@@ -35,7 +33,6 @@ if (!$proveedor) {
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // Obtener los datos del formulario
     $nombre = mysqli_real_escape_string($conexion, $_POST['nombre']);
-
     $telefono = mysqli_real_escape_string($conexion, $_POST['telefono']);
     $correo = mysqli_real_escape_string($conexion, $_POST['correo']);
     $contacto = mysqli_real_escape_string($conexion, $_POST['contacto']);
@@ -52,56 +49,187 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $mensajeError = "Error al editar el proveedor.";
     }
 }
+
+// Mostrar mensaje de error si existe
+if (isset($mensajeError)) {
+    echo "<p>{$mensajeError}</p>";
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Editar Proveedor</title>
+
+    <link rel="icon" href="img/mdb-favicon.ico" type="image/x-icon" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" />
+    <link rel="stylesheet" href="css/mdb.min.css" />
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.22/css/dataTables.bootstrap4.min.css">
 </head>
-<body>
 
-    <?php include('includes/navbar.php'); ?>
+<body class="gradient-custom-1">
 
-    <h1>Editar Proveedor</h1>
+    <?php include ('includes/navbar.php'); ?>
 
-    <?php
-    // Mostrar mensaje de error si existe
-    if (isset($mensajeError)) {
-        echo "<p>{$mensajeError}</p>";
-    }
-    ?>
+    <style>
+        .gradient-custom-1 {
+            height: 100vh;
 
-    <form method="post">
-        <label for="nombre">Nombre:</label>
-        <input type="text" id="nombre" name="nombre" value="<?php echo htmlspecialchars($proveedor['NombreProveedor']); ?>" required>
-        <br>
+            /* fallback for old browsers */
+            background: #EEEEEE;
+        }
 
-        <label for="telefono">Teléfono:</label>
-        <input type="text" id="telefono" name="telefono" value="<?php echo htmlspecialchars($proveedor['Telefono']); ?>" required>
-        <br>
+        .mask-custom {
+            background: rgba(24, 24, 16, .2);
+            border-radius: 2em;
+            backdrop-filter: blur(25px);
+            border: 2px solid rgba(255, 255, 255, 0.05);
+            background-clip: padding-box;
+            box-shadow: 10px 10px 10px rgba(46, 54, 68, 0.03);
+        }
 
-        <label for="correo">Correo Electrónico:</label>
-        <input type="email" id="correo" name="correo" value="<?php echo htmlspecialchars($proveedor['CorreoElectronico']); ?>" required>
-        <br>
+        .near-moon-gradient {
+            background: radial-gradient(circle at 10% 20%, rgb(64, 84, 178) 0%, rgb(219, 2, 234) 90%);
+        }
+    </style>
 
-        <label for="contacto">Contacto:</label>
-        <input type="text" id="contacto" name="contacto" value="<?php echo htmlspecialchars($proveedor['Contacto']); ?>" required>
-        <br>
+    <div class="mx-4 my-4">
+        <a class="btn text-white btn-lg btn-floating" data-mdb-ripple-init style="background-color: #ac2bac;"
+            role="button" onclick="window.location.href='gestionproveedores.php'">
+            <i class="fas fa-angle-left"></i>
+        </a>
 
-        <label for="telefono_contacto">Teléfono de Contacto:</label>
-        <input type="text" id="telefono_contacto" name="telefono_contacto" value="<?php echo htmlspecialchars($proveedor['TelefonoContacto']); ?>" required>
-        <br>
-
-        <label for="nit">NIT:</label>
-        <input type="text" id="nit" name="nit" value="<?php echo htmlspecialchars($proveedor['NIT']); ?>" required>
-        <br>
-
-        <input type="submit" value="Guardar Cambios">
-        <a href="gestionproveedores.php">Cancelar</a>
-    </form>
+        <div class="container">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                    <div class="card near-moon-gradient form-white" style="border-radius: 1rem;">
+                        <div class="card-body p-5">
+                            <form method="POST" action="">
+                                <h3 class="text-center indigo-text font-bold py-4 fw-bold text-uppercase">
+                                    <strong style="color: #fff">Editar Proveedor</strong>
+                                </h3>
+                                <div class="mb-4">
+                                    <div class="d-flex flex-nowrap">
+                                        <div class="order-0 col-md-1 d-flex align-items-center">
+                                            <i class="fas fa-truck prefix white-text" style="color: #fff"></i>
+                                        </div>
+                                        <div data-mdb-input-init class="order-1 form-outline form-white">
+                                            <input type="text" id="nombre" class="form-control form-control-lg"
+                                                name="nombre"
+                                                value="<?php echo htmlspecialchars($proveedor['NombreProveedor']); ?>"
+                                                required>
+                                            <label class="form-label" for="nombre">Nombre del proveedor</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="mb-4">
+                                    <div class="d-flex flex-nowrap">
+                                        <div class="order-0 col-md-1 d-flex align-items-center">
+                                            <i class="far fa-envelope prefix white-text" style="color: #fff"></i>
+                                        </div>
+                                        <div data-mdb-input-init class="order-1 form-outline form-white">
+                                            <input type="email" id="correo" class="form-control form-control-lg"
+                                                name="correo"
+                                                value="<?php echo htmlspecialchars($proveedor['CorreoElectronico']); ?>"
+                                                required>
+                                            <label class="form-label" for="correo">Correo electrónico</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="mb-4">
+                                    <div class="d-flex flex-nowrap">
+                                        <div class="order-0 col-md-1 d-flex align-items-center">
+                                            <i class="fas fa-phone prefix white-text" style="color: #fff"></i>
+                                        </div>
+                                        <div data-mdb-input-init class="order-1 form-outline form-white">
+                                            <input type="text" id="telefono" class="form-control form-control-lg"
+                                                name="telefono"
+                                                value="<?php echo htmlspecialchars($proveedor['Telefono']); ?>"
+                                                required>
+                                            <label class="form-label" for="telefono">Teléfono</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="mb-4">
+                                    <div class="d-flex flex-nowrap">
+                                        <div class="order-0 col-md-1 d-flex align-items-center">
+                                            <i class="far fa-user prefix white-text" style="color: #fff"></i>
+                                        </div>
+                                        <div data-mdb-input-init class="order-1 form-outline form-white">
+                                            <input type="text" id="contacto" class="form-control form-control-lg"
+                                                name="contacto" required
+                                                value="<?php echo htmlspecialchars($proveedor['Contacto']); ?>"
+                                                required>
+                                            <label class="form-label" for="contacto">Nombre de contacto</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="mb-4">
+                                    <div class="d-flex flex-nowrap">
+                                        <div class="order-0 col-md-1 d-flex align-items-center">
+                                            <i class="fas fa-mobile-screen-button prefix white-text"
+                                                style="color: #fff"></i>
+                                        </div>
+                                        <div data-mdb-input-init class="order-1 form-outline form-white">
+                                            <input type="text" id="telefonoContacto"
+                                                class="form-control form-control-lg" name="telefonoContacto"
+                                                value="<?php echo htmlspecialchars($proveedor['TelefonoContacto']); ?>"
+                                                required>
+                                            <label class="form-label" for="telefonoContacto">Teléfono de
+                                                contacto</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="mb-4">
+                                    <div class="d-flex flex-nowrap">
+                                        <div class="order-0 col-md-1 d-flex align-items-center">
+                                            <i class="far fa-address-card white-text" style="color: #fff"></i>
+                                        </div>
+                                        <div data-mdb-input-init class="order-1 form-outline form-white">
+                                            <input type="text" id="nit" class="form-control form-control-lg" name="nit"
+                                                value="<?php echo htmlspecialchars($proveedor['NIT']); ?>" required>
+                                            <label class="form-label" for="nit">NIT</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="d-flex flex-row-reverse justify-content-center">
+                                    <div class="order-0 p-2">
+                                        <button data-mdb-ripple-init type="submit" class="btn btn-light">
+                                            <i class="fas fa-user-plus pe-2"></i>Guardar Cambios</button>
+                                    </div>
+                                    <div class="order-1 p-2">
+                                        <button data-mdb-ripple-init type="button" class="btn btn-danger"
+                                            onclick="window.location.href='gestionproveedores.php'">
+                                            <i class="fas fa-ban pe-2"></i>Cancelar
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 </body>
+
 </html>
+
+<script type="text/javascript" src="js/mdb.umd.min.js"></script>
+<script type="text/javascript"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap4.min.js"></script>
+<script>
+    import { Ripple, initMDB } from "mdb-ui-kit";
+
+    initMDB({ Ripple });
+</script>
