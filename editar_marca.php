414bb0ac60c4457bafcf0fb8ea583b41be3795bc
Juanma320 <juanma320@gmail.com>
2024-04-23 18:02:33 -0500

    Editar_marca Styles

--- editar_marca.php
+++ editar_marca.php
@@ -1,6 +1,14 @@
 <?php
 include ('includes/includes.php');
 include ('includes/funciones.php');
+$row = obtenerDatosUsuario($conexion, $_SESSION['NombreUsuario']);
+
+// Verificar si el usuario tiene el rol de administrador
+if ($_SESSION['RolID'] != 1) {
+    // Si no es administrador, redirigir a la página de inicio
+    header('Location: login.php');
+    exit();
+}
 
 // Obtener el ID de la marca y el ID del proveedor de la URL
 if (isset($_GET['marcaID']) && isset($_GET['proveedorID'])) {
@@ -16,8 +24,8 @@ if (isset($_GET['marcaID']) && isset($_GET['proveedorID'])) {
 
     // Verificar si se encontró la marca
     if (mysqli_num_rows($resultado) > 0) {
-        $row = mysqli_fetch_assoc($resultado);
-        $nombreMarcaActual = $row['NombreMarca'];
+        $row2 = mysqli_fetch_assoc($resultado);
+        $nombreMarcaActual = $row2['NombreMarca'];
     } else {
         echo "No se encontró la marca.";
         exit();
@@ -35,22 +43,107 @@ if (isset($_GET['marcaID']) && isset($_GET['proveedorID'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Editar Marca</title>
+
+    <link rel="icon" href="img/mdb-favicon.ico" type="image/x-icon" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" />
+    <link rel="stylesheet" href="css/mdb.min.css" />
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.22/css/dataTables.bootstrap4.min.css">
 </head>
 
-<body>
+<body class="gradient-custom-1">
 
-    <h1>Editar Marca</h1>
+    <?php include ('includes/navbar.php'); ?>
 
-    <form method="post" action="procesar_editar_marca.php"
-        onsubmit="return confirm('¿Estás seguro de que quieres cambiar el nombre de la marca?');">
-        <input type='hidden' name='marcaID' value='<?php echo $marcaID; ?>'>
-        <input type='hidden' name='proveedorID' value='<?php echo $proveedorID; ?>'>
-        <label for='nuevo_nombre_marca'>Nuevo Nombre de la Marca:</label>
-        <input type='text' id='nuevo_nombre_marca' name='nuevo_nombre_marca' value='<?php echo $nombreMarcaActual; ?>'
-            required>
-        <button type='submit'>Guardar</button>
-    </form>
+    <style>
+        .gradient-custom-1 {
+            height: 100vh;
+
+            /* fallback for old browsers */
+            background: #EEEEEE;
+        }
+
+        .mask-custom {
+            background: rgba(24, 24, 16, .2);
+            border-radius: 2em;
+            backdrop-filter: blur(25px);
+            border: 2px solid rgba(255, 255, 255, 0.05);
+            background-clip: padding-box;
+            box-shadow: 10px 10px 10px rgba(46, 54, 68, 0.03);
+        }
+
+        .near-moon-gradient {
+            background: radial-gradient(circle at 10% 20%, rgb(64, 84, 178) 0%, rgb(219, 2, 234) 90%);
+        }
+    </style>
+
+    <div class="mx-4 my-4">
+        <a class="btn text-white btn-lg btn-floating" data-mdb-ripple-init style="background-color: #ac2bac;"
+            role="button" onclick="window.location.href='marcasproveedor.php?id=<?php echo $proveedorID; ?>'">
+            <i class="fas fa-angle-left"></i>
+        </a>
+
+        <div class="container">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                    <div class="card near-moon-gradient form-white" style="border-radius: 1rem;">
+                        <div class="card-body p-5">
+                            <form method="POST" action="procesar_editar_marca.php"
+                                onsubmit="return confirm('¿Estás seguro de que quieres cambiar el nombre de la marca?');">
+                                <h3 class="text-center indigo-text font-bold py-4 fw-bold text-uppercase">
+                                    <strong style="color: #fff">Editar Marca</strong>
+                                </h3>
+                                <div class="mb-4">
+                                    <div class="d-flex flex-nowrap">
+                                        <div class="order-0 col-md-1 d-flex align-items-center">
+                                            <i class="fa-regular fa-font-awesome prefix white-text"
+                                                style="color: #fff"></i>
+                                        </div>
+                                        <input type='hidden' name='marcaID' value='<?php echo $marcaID; ?>'>
+                                        <input type='hidden' name='proveedorID' value='<?php echo $proveedorID; ?>'>
+                                        <div data-mdb-input-init class="order-1 form-outline form-white">
+                                            <input type="text" id="nuevo_nombre_marca"
+                                                class="form-control form-control-lg" name="nuevo_nombre_marca"
+                                                value='<?php echo $nombreMarcaActual; ?>' required>
+                                            <label class="form-label" for="nuevo_nombre_marca">Nuevo nombre de la
+                                                marca</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="d-flex flex-row-reverse justify-content-center">
+                                    <div class="order-0 p-2">
+                                        <button data-mdb-ripple-init type="submit" class="btn btn-light">
+                                            <i class="fas fa-user-plus pe-2"></i>Guardar Cambios</button>
+                                    </div>
+                                    <div class="order-1 p-2">
+                                        <button data-mdb-ripple-init type="button" class="btn btn-danger"
+                                            onclick="window.location.href='marcasproveedor.php?id=<?php echo $proveedorID; ?>'">
+                                            <i class="fas fa-ban pe-2"></i>Cancelar
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 </body>
 
 </html>
+
+<script type="text/javascript" src="js/mdb.umd.min.js"></script>
+<script type="text/javascript"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap4.min.js"></script>
+<script>
+    import { Ripple, initMDB } from "mdb-ui-kit";
+
+    initMDB({ Ripple });
+</script>
