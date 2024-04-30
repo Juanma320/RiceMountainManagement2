1cb36d81879ad357cb7d9088185900a19fe3d61b
Juanma320 <juanma320@gmail.com>
2024-04-30 16:12:21 -0500

    Perfil Styles

--- perfil.php
+++ perfil.php
@@ -1,6 +1,6 @@
 <?php
-include('includes/includes.php');
-include('includes/funciones.php');
+include ('includes/includes.php');
+include ('includes/funciones.php');
 $row = obtenerDatosUsuario($conexion, $_SESSION['NombreUsuario']);
 // Verificar si el usuario tiene permiso para acceder a esta página
 if ($_SESSION['RolID'] != 1 && $_SESSION['RolID'] != 2 && $_SESSION['RolID'] != 3) {
@@ -15,28 +15,125 @@ $usuario = obtenerDatosUsuario($conexion, $_SESSION['NombreUsuario']);
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Perfil</title>
+
+    <link rel="icon" href="img/mdb-favicon.ico" type="image/x-icon" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" />
+    <link rel="stylesheet" href="css/mdb.min.css" />
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.22/css/dataTables.bootstrap4.min.css">
 </head>
-<body>
 
-    <?php include('includes/navbar.php'); ?>
+<body class="gradient-custom-1">
 
-    <h1>Perfil de Usuario</h1>
-    <p><strong>Nombre de Usuario:</strong> <?php echo $usuario['NombreUsuario']; ?></p>
-    <p><strong>Documento de Identidad:</strong> <?php echo $usuario['DocumentoIdentidad']; ?></p>
-    <p><strong>Rol:</strong> <?php echo obtenerNombreRol($conexion, $usuario['RolID']); ?></p>
-    <p><strong>Correo Electrónico:</strong> <?php echo $usuario['Correo']; ?></p>
-    <a href="editar_perfil.php">Cambiar contraseña</a>
-    <a href="editar_user.php">Editar Perfil</a>
-    <?php
-// Verificar si el usuario tiene permiso de administrador
-if ($_SESSION['RolID'] == 1) {
-    echo '<p><a href="crear_admin.php"><button>Crear Administrador</button></a></p>';
-}
-?>
+    <?php include ('includes/navbar.php'); ?>
+
+    <style>
+        .gradient-custom-1 {
+            height: 100vh;
+
+            /* fallback for old browsers */
+            background: #EEEEEE;
+        }
+
+        .mask-custom {
+            background: rgba(24, 24, 16, .2);
+            border-radius: 2em;
+            backdrop-filter: blur(25px);
+            border: 2px solid rgba(255, 255, 255, 0.05);
+            background-clip: padding-box;
+            box-shadow: 10px 10px 10px rgba(46, 54, 68, 0.03);
+        }
+
+        #tdcenter {
+            display: flex;
+            justify-content: center;
+        }
+
+        .near-moon-gradient {
+            background: radial-gradient(circle at 52.1% -29.6%, rgb(144, 17, 105) 0%, rgb(51, 0, 131) 100.2%);
+        }
+    </style>
+
+    <div class="mx-4 my-4">
+        <a class="btn text-white btn-lg btn-floating" data-mdb-ripple-init style="background-color: #ac2bac;"
+            role="button" href="indexadmin.php">
+            <i class="fas fa-angle-left"></i>
+        </a>
+
+        <div class="container py-5 h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col col-lg-6 mb-4 mb-lg-0">
+                    <div class="card mb-3" style="border-radius: .5rem;">
+                        <div class="row g-0">
+                            <div class="col-md-4 near-moon-gradient text-center text-white"
+                                style="border-top-left-radius: .5rem; border-bottom-left-radius: .5rem;">
+                                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp"
+                                    alt="Avatar" class="img-fluid my-5" style="width: 80px;" />
+                                <h5><?php echo $usuario['NombreUsuario']; ?></h5>
+                                <p><?php echo obtenerNombreRol($conexion, $usuario['RolID']); ?></p>
+                                <a href="editar_user.php" class="text-white"><i class="far fa-edit mb-5"></i></a>
+                            </div>
+                            <div class="col-md-8">
+                                <div class="card-body p-4">
+                                    <h6>Información</h6>
+                                    <hr class="mt-0 mb-4">
+                                    <div class="row pt-1">
+                                        <div class="col-6 mb-3">
+                                            <h6>Email</h6>
+                                            <p class="text-muted"><?php echo $usuario['Correo']; ?></p>
+                                        </div>
+                                        <div class="col-6 mb-3">
+                                            <h6>Documento de Identidad</h6>
+                                            <p class="text-muted"><?php echo $usuario['DocumentoIdentidad']; ?></p>
+                                        </div>
+                                    </div>
+                                    <h6>Opciones</h6>
+                                    <hr class="mt-0 mb-4">
+                                    <div class="row pt-1">
+                                        <div class="col-6 mb-3">
+                                            <a href="editar_perfil.php"><button type="button"
+                                                    class="btn btn-primary btn-rounded" data-mdb-ripple-init>Cambiar
+                                                    contraseña</button></a>
+                                        </div>
+                                        <div class="col-6 mb-3">
+                                            <?php
+                                            // Verificar si el usuario tiene permiso de administrador
+                                            if ($_SESSION['RolID'] == 1) {
+                                                echo '<p><a href="crear_admin.php"><button type="button" class="btn btn-warning btn-rounded" data-mdb-ripple-init>Crear Administrador</button></a></p>';
+                                            }
+                                            ?>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 </body>
+
 </html>
+
+<script type="text/javascript" src="js/mdb.umd.min.js"></script>
+<script type="text/javascript"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap4.min.js"></script>
+<script>
+    // Initialization for ES Users
+    import { Ripple, initMDB } from "mdb-ui-kit";
+
+    initMDB({ Ripple });
+</script>
