e64e4f732b76af88368310ee724bcec1dd82ca0f
Juanma320 <juanma320@gmail.com>
2024-05-02 17:54:17 -0500

    Crear_admin Styles

--- crear_admin.php
+++ crear_admin.php
@@ -1,6 +1,6 @@
 <?php
-include('includes/includes.php');
-include('includes/funciones.php');
+include ('includes/includes.php');
+include ('includes/funciones.php');
 $row = obtenerDatosUsuario($conexion, $_SESSION['NombreUsuario']);
 // Verificar si el usuario tiene permiso para acceder a esta página
 if ($_SESSION['RolID'] != 1) {
@@ -38,37 +38,149 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Crear Administrador</title>
+
+    <link rel="icon" href="img/mdb-favicon.ico" type="image/x-icon" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" />
+    <link rel="stylesheet" href="css/mdb.min.css" />
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.22/css/dataTables.bootstrap4.min.css">
 </head>
-<body>
 
-    <?php include('includes/navbar.php'); ?>
+<body class="gradient-custom-1">
 
-    <h1>Crear Administrador</h1>
+    <?php include ('includes/navbar.php'); ?>
 
-    <?php if (isset($mensajeError)): ?>
-        <p style="color: red;"><?php echo $mensajeError; ?></p>
-    <?php endif; ?>
+    <style>
+        .gradient-custom-1 {
+            height: 100vh;
 
-    <form method="POST" action="">
-        <label for="nombreUsuario">Nombre de Usuario:</label><br>
-        <input type="text" id="nombreUsuario" name="nombreUsuario" required><br>
+            /* fallback for old browsers */
+            background: #EEEEEE;
+        }
 
-        <label for="correo">Correo Electrónico:</label><br>
-        <input type="email" id="correo" name="correo" required><br>
+        .mask-custom {
+            background: rgba(24, 24, 16, .2);
+            border-radius: 2em;
+            backdrop-filter: blur(25px);
+            border: 2px solid rgba(255, 255, 255, 0.05);
+            background-clip: padding-box;
+            box-shadow: 10px 10px 10px rgba(46, 54, 68, 0.03);
+        }
 
-        <label for="documentoIdentidad">Documento de Identidad:</label><br>
-        <input type="text" id="documentoIdentidad" name="documentoIdentidad" required><br>
+        .near-moon-gradient {
+            background: radial-gradient(circle at 10% 20%, rgb(64, 84, 178) 0%, rgb(219, 2, 234) 90%);
+        }
+    </style>
 
-        <label for="contrasena">Contraseña:</label><br>
-        <input type="password" id="contrasena" name="contrasena" required><br>
+    <div class="mx-4 my-4">
+        <a class="btn text-white btn-lg btn-floating" data-mdb-ripple-init style="background-color: #ac2bac;"
+            role="button" onclick="window.location.href='perfil.php'">
+            <i class="fas fa-angle-left"></i>
+        </a>
 
-        <input type="submit" value="Crear Administrador">
-        <a href="perfil.php">Cancelar</a>
-    </form>
+        <div class="container">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                    <div class="card near-moon-gradient form-white" style="border-radius: 1rem;">
+                        <div class="card-body p-5">
+                            <form method="POST" action="">
+                                <h3 class="text-center indigo-text font-bold py-4 fw-bold text-uppercase">
+                                    <strong style="color: #fff">Crear administrador</strong>
+                                </h3>
+
+                                <?php if (isset($mensajeError)): ?>
+                                    <p style="color: red;"><?php echo $mensajeError; ?></p>
+                                <?php endif; ?>
+
+                                <div class="mb-4">
+                                    <div class="d-flex flex-nowrap">
+                                        <div class="order-0 col-md-1 d-flex align-items-center">
+                                            <i class="far fa-user prefix white-text" style="color: #fff"></i>
+                                        </div>
+                                        <div data-mdb-input-init class="order-1 form-outline form-white">
+                                            <input type="text" id="nombreUsuario" class="form-control form-control-lg"
+                                                name="nombreUsuario" required />
+                                            <label class="form-label" for="nombreUsuario">Nombre de usuario</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="mb-4">
+                                    <div class="d-flex flex-nowrap">
+                                        <div class="order-0 col-md-1 d-flex align-items-center">
+                                            <i class="fa-regular fa-eye-slash white-text" style="color: #fff"></i>
+                                        </div>
+                                        <div data-mdb-input-init class="order-1 form-outline form-white">
+                                            <input type="password" id="contrasena" class="form-control form-control-lg"
+                                                name="contrasena" required />
+                                            <label class="form-label" for="contrasena">Contraseña</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="mb-4">
+                                    <div class="d-flex flex-nowrap">
+                                        <div class="order-0 col-md-1 d-flex align-items-center">
+                                            <i class="far fa-address-card white-text" style="color: #fff"></i>
+                                        </div>
+                                        <div data-mdb-input-init class="order-1 form-outline form-white">
+                                            <input type="text" id="documentoIdentidad"
+                                                class="form-control form-control-lg" name="documentoIdentidad" />
+                                            <label class="form-label" for="documentoIdentidad">Documento de
+                                                identidad</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="mb-4">
+                                    <div class="d-flex flex-nowrap">
+                                        <div class="order-0 col-md-1 d-flex align-items-center">
+                                            <i class="far fa-envelope prefix white-text" style="color: #fff"></i>
+                                        </div>
+                                        <div data-mdb-input-init class="order-1 form-outline form-white">
+                                            <input type="email" id="correo" class="form-control form-control-lg"
+                                                name="correo" required />
+                                            <label class="form-label" for="correo">Correo electrónico</label>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="d-flex flex-row-reverse justify-content-center">
+                                    <div class="order-0 p-2">
+                                        <button data-mdb-ripple-init type="submit" class="btn btn-light">
+                                            <i class="fas fa-user-plus pe-2"></i>Crear Administrador</button>
+                                    </div>
+                                    <div class="order-1 p-2">
+                                        <button data-mdb-ripple-init type="button" class="btn btn-danger"
+                                            onclick="window.location.href='perfil.php'">
+                                            <i class="fas fa-ban pe-2"></i>Cancelar
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 </body>
+
 </html>
+
+<script type="text/javascript" src="js/mdb.umd.min.js"></script>
+<script type="text/javascript"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap4.min.js"></script>
+<script>
+    import { Ripple, initMDB } from "mdb-ui-kit";
+
+    initMDB({ Ripple });
+</script>
