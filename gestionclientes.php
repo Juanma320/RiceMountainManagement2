69ff100c8acbb538097d316b83c646bec221ed96
Juanma320 <juanma320@gmail.com>
2024-05-07 16:46:38 -0500

    Gestionclientes Styles

--- gestionclientes.php
+++ gestionclientes.php
@@ -1,11 +1,11 @@
 <?php
-include('includes/includes.php');
-include('includes/funciones.php');
+include ('includes/includes.php');
+include ('includes/funciones.php');
 $row = obtenerDatosUsuario($conexion, $_SESSION['NombreUsuario']);
 $rolUsuario = obtenerRolUsuario($conexion, $_SESSION['NombreUsuario']);
-
-if ($rolUsuario != 1) {
-    header("Location: login.php");
+// Verificar si el usuario tiene el rol de administrador
+if ($_SESSION['RolID'] != 1) {
+    header('Location: login.php');
     exit();
 }
 
@@ -13,45 +13,130 @@ $queryClientes = "SELECT C.*, U.NombreUsuario as NombreCoordinador
                   FROM Clientes C
                   LEFT JOIN Usuarios U ON C.CoordinadorID = U.UsuarioID";
 $resultClientes = mysqli_query($conexion, $queryClientes);
-
 ?>
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gestionar Clientes</title>
-</head>
-<body>
-<?php include('includes/navbar.php'); ?>
-    <h1>Gestionar Clientes</h1>
-    <a href="agregarcliente.php">Agregar Cliente</a>
-    <table border="1">
-        <tr>
-            <th>Nombre Cliente</th>
-            <th>Correo Electrónico</th>
-            <th>Teléfono</th>
-            <th>Coordinador</th>
-            <th>Nombre Encargado</th>
-            <th>Acciones</th>
-            <th>Agregar Dirección</th>
-        </tr>
 
-        <?php
-        while ($rowCliente = mysqli_fetch_assoc($resultClientes)) {
-            echo "<tr>";
-            echo "<td><a href='infocliente.php?clienteID={$rowCliente['ClienteID']}'>{$rowCliente['NombreCliente']}</a></td>";
-            echo "<td>{$rowCliente['CorreoElectronico']}</td>";
-            echo "<td>{$rowCliente['Telefono']}</td>";
-            echo "<td>{$rowCliente['NombreCoordinador']}</td>";
-            echo "<td>{$rowCliente['NombreEncargado']}</td>";
-            echo "<td><a href='editarcliente.php?clienteID={$rowCliente['ClienteID']}'>Editar</a></td>";
-            echo "<td><a href='agregardireccion.php?clienteID={$rowCliente['ClienteID']}'>Agregar Dirección</a></td>";
-            echo "</tr>";
+    <link rel="icon" href="img/mdb-favicon.ico" type="image/x-icon" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" />
+    <link rel="stylesheet" href="css/mdb.min.css" />
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.22/css/dataTables.bootstrap4.min.css">
+</head>
+
+<body class="gradient-custom-1">
+
+    <?php include ('includes/navbar.php'); ?>
+
+    <style>
+        .gradient-custom-1 {
+            height: 100vh;
+
+            /* fallback for old browsers */
+            background: #EEEEEE;
         }
-        ?>
-    </table>
+
+        .mask-custom {
+            background: rgba(24, 24, 16, .2);
+            border-radius: 2em;
+            backdrop-filter: blur(25px);
+            border: 2px solid rgba(255, 255, 255, 0.05);
+            background-clip: padding-box;
+            box-shadow: 10px 10px 10px rgba(46, 54, 68, 0.03);
+        }
+
+        #tdcenter {
+            display: flex;
+            justify-content: center;
+        }
+    </style>
+
+    <div class="mx-4 my-4">
+        <a class="btn text-white btn-lg btn-floating" data-mdb-ripple-init style="background-color: #ac2bac;"
+            role="button" href="indexadmin.php">
+            <i class="fas fa-angle-left"></i>
+        </a>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="d-flex justify-content-between align-items-center">
+                        <h1>Gestionar Clientes</h1>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="row mt-4">
+                <div class="col-md-12">
+                    <ul class="nav nav-pills flex-column flex-md-row">
+                        <li class="nav-item">
+                            <a href="agregarcliente.php" style="color: #F9F6EE">
+                                <button data-mdb-ripple-init type="button" class="btn btn-primary btn-rounded">
+                                    <i class="fas fa-user pe-2"></i>Agregar cliente</button>
+                            </a>
+                        </li>
+                    </ul>
+
+                    <div class="row mt-4">
+                        <div class="col-md-12">
+                            <table class="table table-striped table-responsive rounded-9 overflow-hidden table-hover"
+                                id="sortTable">
+                                <thead class="table-dark">
+                                    <tr>
+                                        <th>Nombre Cliente</th>
+                                        <th>Correo Electrónico</th>
+                                        <th>Teléfono</th>
+                                        <th>Coordinador</th>
+                                        <th>Nombre Encargado</th>
+                                        <th class="text-center">Modificar Cliente</th>
+                                        <th class="text-center">Agregar Dirección</th>
+                                    </tr>
+                                </thead>
+                                <?php
+                                // Iterar sobre los resultados y mostrar en la tabla
+                                while ($rowCliente = mysqli_fetch_assoc($resultClientes)) {
+                                    echo "<tr>";
+                                    echo "<td><a data-mdb-ripple-init data-mdb-ripple-color='dark' class='btn btn-outline-dark' href='infocliente.php?clienteID={$rowCliente['ClienteID']}'>{$rowCliente['NombreCliente']}</a></td>";
+                                    echo "<td>{$rowCliente['CorreoElectronico']}</td>";
+                                    echo "<td>{$rowCliente['Telefono']}</td>";
+                                    echo "<td>{$rowCliente['NombreCoordinador']}</td>";
+                                    echo "<td>{$rowCliente['NombreEncargado']}</td>";
+                                    echo "<td><a class='text-center'><a data-mdb-ripple-init class='btn btn-warning' href='editarcliente.php?clienteID={$rowCliente['ClienteID']}'>Modificar</a></td>";
+                                    echo "<td><a class='text-center'><a data-mdb-ripple-init class='btn btn-primary' href='agregardireccion.php?clienteID={$rowCliente['ClienteID']}'>Agregar</a></td>";
+                                    echo "</tr>";
+                                }
+                                ?>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </body>
+
 </html>
 
+<script type="text/javascript" src="js/mdb.umd.min.js"></script>
+<script type="text/javascript"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap4.min.js"></script>
+<script>$('#sortTable').DataTable({ order: [[0, 'asc']] })</script>
+<script>
+    // Initialization for ES Users
+    import { Ripple, initMDB } from "mdb-ui-kit";
+
+    initMDB({ Ripple });
+</script>
